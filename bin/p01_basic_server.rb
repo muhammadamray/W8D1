762d26612b0ebd4c6d2919aa864c180ef0936fae
Muhammad Amray <muhammadamray@gmail.com>
2023-08-14 16:22:52 -0400

    phase 1 complete

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,14 @@
-require 'rack'
+require 'rack' 
+
+
+  app = Proc.new do |env|
+    ['200', {'Content-Type' => 'text/html'}, ['hello kyle']]
+  end
+
+
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
+
